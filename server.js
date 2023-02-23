308c01f61a5173d0e7ae4f3be6049245c21134c2
Master0229 <masterservice2020@gmail.com>
2023-02-23 11:44:00 +0530

    #23-02-2023 2nd commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/ChitFund'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/ChitFund/index.html'));});
+app.listen(process.env.PORT || 8080);
